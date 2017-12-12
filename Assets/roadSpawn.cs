491508daf4a6b2053926d5a1714cce18cfe678de
JPOstholt <jascha@kreait.com>
2017-12-12 13:20:14 +0100

    Initial commit

---
+++ Assets/roadSpawn.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roadSpawn : MonoBehaviour {
+
+
+
+	public GameObject nextSpawn;
+	int randomRoadNumber;
+	public List<GameObject> roadTiles;
+	public GameObject roadManager;
+	public GameObject bombPrefab;
+	Vector3 newPosition;
+	int bombCount;
+	public int bombMax;
+	public Vector3 bombPos;
+	public GameObject currentRoad;
+	public GameObject lastRoad;
+	// Use this for initialization
+	void Start () {
+
+
+
+		roadManager = GameObject.FindGameObjectWithTag ("roadManager");
+
+		roadTiles = roadManager.GetComponent<roadManager> ().roadTiles;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+
+
+
+
+		if (other.gameObject.name == "Player") {
+
+
+
+			Debug.Log ("bepis");
+
+			randomRoadNumber = Random.Range (0, roadTiles.Count);
+
+
+			newPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 200);
+
+			currentRoad = Instantiate (roadTiles [randomRoadNumber], newPosition, Quaternion.identity);
+			currentRoad.GetComponent<roadSpawn> ().lastRoad = this.gameObject;
+
+			bombCount = Random.Range (0, bombMax);
+
+			for (int i = 0; i < bombCount; i++) {
+
+
+				bombPos = new Vector3 (transform.position.x + Random.Range(-560,50), transform.position.y +  Random.Range(80,150), transform.position.z + Random.Range(-100,100));
+
+				GameObject currentBomb = Instantiate (bombPrefab, bombPos, Quaternion.Euler(Random.Range(0,360), Random.Range(0,360), Random.Range(0,360)));
+				currentBomb.GetComponent<Rigidbody> ().AddForce (Vector3.down * 2000);
+				currentBomb.transform.SetParent (transform);
+			}
+
+			GetComponent<BoxCollider> ().enabled = false;
+
+			Destroy (lastRoad.gameObject);
+
+		}
+
+	}
+}
---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+	public float xInput;
+	public float zInput;
+	[Range(0,100)]
+	public float moveSpeed;
+	public float jumpForce;
+	public int jumps = 0;
+	public int maxJumps = 1;
+	public float groundCheckRadius;
+	public LayerMask ground;
+	public float airControl;
+	public Vector3 velocity;
+	public int maxSpeed;
+	public bool nitro;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		velocity = GetComponent<Rigidbody> ().velocity;
+
+		xInput = Input.GetAxis ("Horizontal");
+		zInput = Input.GetAxis ("Vertical");
+
+		if ((Mathf.Abs (velocity.x) < maxSpeed) ) {
+
+			moveX ();
+
+		} 
+		if (( Mathf.Abs (velocity.z) < maxSpeed)) {
+			
+			moveZ ();
+		}
+
+		if ((Input.GetKeyDown(KeyCode.JoystickButton0)|| Input.GetKeyDown (KeyCode.Space) ) && jumps > 0) {
+			GetComponent<Rigidbody> ().AddForce (new Vector3 (0, jumpForce, 0));
+			jumps--;
+		}
+
+
+		if (Physics.OverlapSphere (transform.position, groundCheckRadius, ground).Length > 0) {
+
+			jumps = maxJumps;
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.JoystickButton1)) {
+			if (nitro) {
+				GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0, 2000));
+				nitro = false;
+				StartCoroutine ("nitroTime");
+			}
+		}
+
+		if (nitro) {
+
+			//GetComponent<MeshRenderer> ().material.color = Color.cyan;
+
+		} else {
+			//GetComponent<MeshRenderer> ().material.color = Color.white;
+
+		}
+
+	}
+
+ 	IEnumerator nitroTime(){
+
+		//yield return new WaitForSeconds (3);
+
+		//nitro = false;
+
+		yield return new WaitForSeconds (5);
+
+		nitro = true;
+
+	}
+
+	void moveX () {
+		if (jumps > 0) {
+			GetComponent<Rigidbody> ().AddForce (new Vector3 (xInput, 0,0) * moveSpeed);
+		} else {
+
+			GetComponent<Rigidbody> ().AddForce (new Vector3 (xInput, 0, 0) * moveSpeed * airControl);
+		}
+	}
+
+	void moveZ () {
+		if (jumps > 0) {
+			GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0, zInput) * moveSpeed);
+		} else {
+
+			GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0, zInput) * moveSpeed * airControl);
+		}
+	}
+
+
+
+}
